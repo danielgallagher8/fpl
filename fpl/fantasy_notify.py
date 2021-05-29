f2d55e9c9930b3bc29f0628c6f3e587b87a2f2ec
danielgallagher8 <51876055+danielgallagher8@users.noreply.github.com>
2021-05-29 17:58:44 +0100

    Update fantasy_notify.py

--- fpl/fantasy_notify.py
+++ fpl/fantasy_notify.py
@@ -1,1 +1,11 @@
+"""
+Send automated emails about fantasy deadline
+"""
 
+import ultimate_fantasy as f
+import email_notifs as e
+
+deadline = f.fpl_players()._get_deadline()
+format_deadline = str(deadline.hour) + ':' + str(deadline.minute) + ' ' + str(deadline.day) + '-' + str(deadline.month) + '-' + str(deadline.year)
+message = 'The next deadline to make changes to you team for Fantasy Football is:<br><br>{}<br><br>https://fantasy.premierleague.com/'.format(format_deadline)
+send_mail = e.notif(msg=message, subject='Next Fantasy Football Deadline')
