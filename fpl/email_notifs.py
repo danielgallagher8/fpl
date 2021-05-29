deb46251a7a0829c99466554afdaa3b50e8d78e2
danielgallagher8 <51876055+danielgallagher8@users.noreply.github.com>
2021-05-29 17:55:24 +0100

    Update email_notifs.py

--- fpl/email_notifs.py
+++ fpl/email_notifs.py
@@ -1,1 +1,38 @@
+"""
+Email notification system for trading app
+"""
 
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+receiver_email = ''
+sender_email = ''
+password = ''
+    
+def notif(msg, sender_email=sender_email, receiver_email=receiver_email, subject='Stock Alert', text='text'):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    
+    if text == 'text':
+        html = """\
+        <html>
+        <head>
+        </head>
+        <body>
+        {0}
+        </body>
+        </html>""".format(msg)
+    elif text == 'html':
+        html = msg
+    
+    part = MIMEText(html, "html")
+    message.attach(part)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+                        sender_email, receiver_email, message.as_string()
+        )
